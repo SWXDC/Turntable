d905d4d562f4bd500a812ca92fb1c7b3e60776e6
Waste_base <xuczxc100@gmail.com>
2024-09-26 13:11:15 +0800

    Beta1.0.2

--- 抽人轉盤/抽人轉盤/obj/Debug/net8.0-windows/抽人轉盤.AssemblyInfo.cs
+++ 抽人轉盤/抽人轉盤/obj/Debug/net8.0-windows/抽人轉盤.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("抽人轉盤")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c88613d73d8db24752ba75ced5072ffd2cab023")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d061ecfd5b5a914e53fabcc5cc417e7a006db7b")]
 [assembly: System.Reflection.AssemblyProductAttribute("抽人轉盤")]
 [assembly: System.Reflection.AssemblyTitleAttribute("抽人轉盤")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
